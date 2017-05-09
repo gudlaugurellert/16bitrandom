c47e2ecf113176ddf4db704ba904d6d6bad6013f
Gudlaugur Ellert <geb@geb.is>
2017-05-09 16:46:06 +1000

    Started to figure out how to use semaphores. My struct is now implemented with semaphores. Am now working on figuring out how to use the locks and unlocks.

--- Milestone2/semaphore/Sources/main.swift
+++ Milestone2/semaphore/Sources/main.swift
@@ -17,16 +17,26 @@ struct InputStruct {
 //  var childLock_2: UnsafeMutablePointer<pthread_mutex_t>
 //  var enterLock_3: UnsafeMutablePointer<pthread_mutex_t>
   
+//  var sem1: SemaModule = SemaModule(value: 1)
+//  var sem2: SemaModule = SemaModule(value: 1)
+  
+  var sem1: UnsafeMutablePointer<SemaModule>
+  var sem2: UnsafeMutablePointer<SemaModule>
+  
   var inputBuffer: UnsafeMutablePointer<String>
   
-  init(_ mutex1: UnsafeMutablePointer<pthread_mutex_t>,
+  init(/*_ mutex1: UnsafeMutablePointer<pthread_mutex_t>,
        _ mutex2: UnsafeMutablePointer<pthread_mutex_t>,
-       _ mutex3: UnsafeMutablePointer<pthread_mutex_t>,
-       _ textInput: UnsafeMutablePointer<String>) {
+       _ mutex3: UnsafeMutablePointer<pthread_mutex_t>,*/
+        _ semaphore1: UnsafeMutablePointer<SemaModule>,
+        _ semaphore2: UnsafeMutablePointer<SemaModule>,
+        _ textInput: UnsafeMutablePointer<String>) {
     
-    self.mainLock_1 = mutex1
-    self.childLock_2 = mutex2
-    self.enterLock_3 = mutex3
+//    self.mainLock_1 = mutex1
+//    self.childLock_2 = mutex2
+//    self.enterLock_3 = mutex3
+    self.sem1 = semaphore1
+    self.sem2 = semaphore2
     self.inputBuffer = textInput
   }
 }
@@ -45,41 +55,44 @@ func repeatFunc(input: UnsafeMutableRawPointer) -> UnsafeMutableRawPointer? {
   typealias StructP = UnsafeMutablePointer<InputStruct>
   let sp: StructP = temp.assumingMemoryBound(to: InputStruct.self)
   
-  pthread_mutex_lock(sp.pointee.childLock_2)
-  pthread_mutex_lock(sp.pointee.mainLock_1)
+//  pthread_mutex_lock(sp.pointee.childLock_2)
+//  pthread_mutex_lock(sp.pointee.mainLock_1)
   
   // print("printing input:") /* For Debugging */
   // Printing out the input from stdin stored in my struct
   print(sp.pointee.inputBuffer.pointee)
   
-  pthread_mutex_unlock(sp.pointee.childLock_2)
-  pthread_mutex_lock(sp.pointee.enterLock_3)
-  
-  pthread_mutex_unlock(sp.pointee.mainLock_1)
-  pthread_mutex_unlock(sp.pointee.childLock_2)
-  pthread_mutex_unlock(sp.pointee.enterLock_3)
+//  pthread_mutex_unlock(sp.pointee.childLock_2)
+//  pthread_mutex_lock(sp.pointee.enterLock_3)
+//  
+//  pthread_mutex_unlock(sp.pointee.mainLock_1)
+//  pthread_mutex_unlock(sp.pointee.childLock_2)
+//  pthread_mutex_unlock(sp.pointee.enterLock_3)
   
   print("Exiting repeatFunc") /* For Debugging */
   
   return nil
 }
 
-// Create mutexes
-var mainLock_1 = pthread_mutex_t()
-var childLock_2 = pthread_mutex_t()
-var enterLock_3 = pthread_mutex_t()
 
-// Create attributes
-var attr1 = pthread_mutexattr_t()
-var attr2 = pthread_mutexattr_t()
-var attr3 = pthread_mutexattr_t()
+//// Create mutexes
+//var mainLock_1 = pthread_mutex_t()
+//var childLock_2 = pthread_mutex_t()
+//var enterLock_3 = pthread_mutex_t()
+//
+//// Create attributes
+//var attr1 = pthread_mutexattr_t()
+//var attr2 = pthread_mutexattr_t()
+//var attr3 = pthread_mutexattr_t()
+//
+//// Initializing attributes
+//// !!!!!! FUN FACT, THIS IS WHAT I FORGOT TO DO FROM THE VERY BEGINNING !!!!!!
+//var a1: Int32 = pthread_mutexattr_init(&attr1)
+//var a2: Int32 = pthread_mutexattr_init(&attr2)
+//var a3: Int32 = pthread_mutexattr_init(&attr3)
 
-// Initializing attributes
-// !!!!!! FUN FACT, THIS IS WHAT I FORGOT TO DO FROM THE VERY BEGINNING !!!!!!
-var a1: Int32 = pthread_mutexattr_init(&attr1)
-var a2: Int32 = pthread_mutexattr_init(&attr2)
-var a3: Int32 = pthread_mutexattr_init(&attr3)
-
+/*
+ 
 // Error handling
 if (a1 != 0) {
   errorHandler(no: a1, msg: "mutex attribute init: a1")
@@ -93,11 +106,15 @@ if (a3 != 0) {
   errorHandler(no: a3, msg: "mutex attribute init: a3")
 }
 
+*/
+
 // Initializing mutexes
-var m1: Int32 = pthread_mutex_init(&mainLock_1, &attr1)
-var m2: Int32 = pthread_mutex_init(&childLock_2, &attr2)
-var m3: Int32 = pthread_mutex_init(&enterLock_3, &attr3)
+//var m1: Int32 = pthread_mutex_init(&mainLock_1, &attr1)
+//var m2: Int32 = pthread_mutex_init(&childLock_2, &attr2)
+//var m3: Int32 = pthread_mutex_init(&enterLock_3, &attr3)
 // Could also just have done pthread_mutex_init(&lockX, nil)
+
+/*
 
 // Error handling
 if (m1 != 0) {
@@ -111,18 +128,27 @@ if (m2 != 0) {
 if (m3 != 0) {
   errorHandler(no: m3, msg: "mutex init: m3")
 }
+ 
+*/
 
 // Locking first mutex for main thread
-pthread_mutex_lock(&mainLock_1)
+//pthread_mutex_lock(&mainLock_1)
+
+
+var sem1: SemaModule = SemaModule(value: 1)
+var sem2: SemaModule = SemaModule(value: 1)
 
 var inputBuffer: String = ""
 
 // Constructing my struct stored values
-var structArgs: InputStruct = InputStruct(&mainLock_1, &childLock_2, &enterLock_3, &inputBuffer)
+var structArgs: InputStruct = InputStruct(/*&mainLock_1, &childLock_2, &enterLock_3,*/&sem1, &sem2, &inputBuffer)
 
 var pt: pthread_t?
 
 // print("create new child thread") /* For Debugging */
+
+sem1.procure()
+sem2.procure()
 
 // Creating a new thread, passing in the address of the struct
 var s: Int32 = pthread_create(&pt, nil, repeatFunc, &structArgs)
@@ -130,12 +156,12 @@ var s: Int32 = pthread_create(&pt, nil, repeatFunc, &structArgs)
 // Reading in the input
 if let stdin = readLine() {
   //  print("first input readline") /* For Debugging */
-  pthread_mutex_lock(&enterLock_3)
+//  pthread_mutex_lock(&enterLock_3)
   inputBuffer = stdin
 }
 
-pthread_mutex_unlock(&mainLock_1) // Allows repeatFunc to print input
-pthread_mutex_lock(&childLock_2) // Makes main wait for repeatFunc to unlock/finish
+//pthread_mutex_unlock(&mainLock_1) // Allows repeatFunc to print input
+//pthread_mutex_lock(&childLock_2) // Makes main wait for repeatFunc to unlock/finish
 
 print("Press Enter to Quit: ")
 
@@ -144,8 +170,8 @@ if let stdin = readLine() {
   
 }
 
-pthread_mutex_unlock(&enterLock_3)
-pthread_mutex_unlock(&childLock_2)
+//pthread_mutex_unlock(&enterLock_3)
+//pthread_mutex_unlock(&childLock_2)
 
 print("Child is exiting")
 
@@ -169,6 +195,6 @@ if (status != 0) {
 }
 
 // Destroy the mutexes to clear up resources
-pthread_mutex_destroy(&mainLock_1)
-pthread_mutex_destroy(&childLock_2)
-pthread_mutex_destroy(&enterLock_3)
+//pthread_mutex_destroy(&mainLock_1)
+//pthread_mutex_destroy(&childLock_2)
+//pthread_mutex_destroy(&enterLock_3)
