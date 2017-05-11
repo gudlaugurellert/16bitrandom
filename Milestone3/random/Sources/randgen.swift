ccb4aa681ae76682e02915cced53bede5551afb9
Gudlaugur Ellert <geb@geb.is>
2017-05-11 10:26:51 +1000

    Started Milestone3. Setting up environment

---
+++ Milestone3/random/Sources/randgen.swift
@@ -0,0 +1,11 @@
+/*
+ Author: Eli Birgisson
+ s2956190
+ Griffith University
+ Trimester 1, 2017
+ 3420ICT Systems Programming
+ Assignment 2
+ Milestone 3 - Random 16bit Number Generator
+ */
+
+import Foundation
